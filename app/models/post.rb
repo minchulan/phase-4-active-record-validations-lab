00dcfa5de4fa6772faf3f47f09ae5a24acecb887
Minchul An <min@minchulan.io>
2023-04-09 22:44:43 -0600

    commit tree de296cc0b0fb2396b4f3241aa4442f9c023b899a

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true 
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+
+    validate :title_is_sufficiently_clickbaity 
+
+    private 
+    def title_is_sufficiently_clickbaity
+        if title != "You Won't Believe These True Facts"
+            errors.add(:title, "title is not sufficiently clickbait-y")
+        end 
+    end 
 end
